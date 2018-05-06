228a38189264995f713a31fb8b29870a094eb927
JamesClonk <jamesclonk@jamesclonk.ch>
2018-05-06 10:47:33 +0200

    mirroring

--- app.sh
+++ app.sh
@@ -4,5 +4,38 @@
 set -e
 
 # =============================================================================================
+echo "parsing VCAP_SERVICES, looking for [${SOURCE_SERVICE_NAME}] and [${TARGET_SERVICE_NAME}] ..."
+export SOURCE_CREDENTIALS=$(echo ${VCAP_SERVICES} | ./jq '.. | select(.name? == "'${SOURCE_SERVICE_NAME}'") | .credentials')
+export SOURCE_HOST=$(echo ${SOURCE_CREDENTIALS} | ./jq -r '.accessHost')
+export SOURCE_KEY=$(echo ${SOURCE_CREDENTIALS} | ./jq -r '.accessKey')
+export SOURCE_SECRET=$(echo ${SOURCE_CREDENTIALS} | ./jq -r '.sharedSecret')
+export TARGET_CREDENTIALS=$(echo ${VCAP_SERVICES} | ./jq '.. | select(.name? == "'${TARGET_SERVICE_NAME}'") | .credentials')
+export TARGET_HOST=$(echo ${TARGET_CREDENTIALS} | ./jq -r '.accessHost')
+export TARGET_KEY=$(echo ${TARGET_CREDENTIALS} | ./jq -r '.accessKey')
+export TARGET_SECRET=$(echo ${TARGET_CREDENTIALS} | ./jq -r '.sharedSecret')
+
+# =============================================================================================
+echo "writing mc-config ..."
+echo '{
+	"version": "9",
+	"hosts": {
+		"source": {
+			"url": "https://'${SOURCE_HOST}'",
+			"accessKey": "'${SOURCE_KEY}'",
+			"secretKey": "'${SOURCE_SECRET}'",
+			"api": "S3v4",
+			"lookup": "auto"
+		},
+		"target": {
+			"url": "https://'${TARGET_HOST}'",
+			"accessKey": "'${TARGET_KEY}'",
+			"secretKey": "'${TARGET_SECRET}'",
+			"api": "S3v4",
+			"lookup": "auto"
+		}
+	}
+}' > config.json
+
+# =============================================================================================
 echo "mirroring buckets ..."
-mc mirror --force --watch
+./mc -C . mirror --overwrite --watch -a source/ target/
--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ set -e
 echo "downloading minio client ..."
 wget https://dl.minio.io/client/mc/release/linux-amd64/mc -O mc
 chmod +x mc
+
+echo "downloading jq ..."
+wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O jq
+chmod +x jq
